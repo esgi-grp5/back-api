dbeb1867a2af0eaaae1a298b355c2ae97ca86f6f
foucaud_e <foucauden@gmail.com>
2022-07-05 10:47:28 +0200

    Refactor: Routes hello to health

--- server/routes.go
+++ server/routes.go
@@ -3,7 +3,7 @@ package main
 func (s *server) routes() {
 	// Routes
 	s.gin.POST("/oauth", s.OAuth)
-	s.gin.GET("/hello", s.HealthCheck)
+	s.gin.GET("/health", s.HealthCheck)
 	// Access with OAuth
 	s.gin.GET("/flutter", s.FlutterAccess)
 }
