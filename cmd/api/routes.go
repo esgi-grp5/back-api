a0a7c6bd04d541f1cc971d2699594a9fbc8081b8
foucaud_e <efoucaud@ifotec.com>
2022-07-14 15:04:27 +0200

    Refactor: Comment

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,18 +1,28 @@
 package main
 
 func (s *server) routes() {
-	// Access without OAuth
+	/* Access without OAuth */
 	s.gin.POST("/oauth", s.OAuth)
 	s.gin.GET("/health", s.HealthCheck)
-	// Access with OAuth
+	/* Access with OAuth */
 	oauth := s.gin.Use(s.OAuthMiddleware)
 	oauth.GET("/hello", s.FlutterAccess)
 	oauth.POST("/login", s.Login)
 	oauth.POST("/register", s.Register)
+	// Movie
 	oauth.GET("/movie/wishlist", s.GetMovieWishList)
 	oauth.POST("/movie/wishlist", s.AddMovieWishList)
 	oauth.DELETE("/movie/wishlist", s.DeleteMovieWishList)
-	// oauth.GET("/serie/wishlist", s.GetMovie)
-	// oauth.GET("/game/wishlist", s.GetMovie)
-	// oauth.GET("/music/wishlist", s.GetMovie)
+	// Serie
+	// oauth.GET("/serie/wishlist", s.GetSerieWishList)
+	// oauth.POST("/serie/wishlist", s.AddSerieWishList)
+	// oauth.DELETE("/serie/wishlist", s.DeleteSerieWishList)
+	// Game
+	// oauth.GET("/game/wishlist", s.GetGameWishList)
+	// oauth.POST("/game/wishlist", s.AddGameWishList)
+	// oauth.DELETE("/game/wishlist", s.DeleteGameWishList)
+	// Music
+	// oauth.GET("/music/wishlist", s.GetMusicWishList)
+	// oauth.POST("/music/wishlist", s.AddMusicWishList)
+	// oauth.DELETE("/music/wishlist", s.DeleteMusicWishList)
 }
